0ee8bfb3e026023e652be5c915033fa7d8e85700
justinmutinta <justinmutinta@hotmail.com>
2023-10-06 21:04:44 -0500

    Created table

--- python_files/DatabaseManagement/Database.py
+++ python_files/DatabaseManagement/Database.py
@@ -3,3 +3,13 @@ import sqlite3
 conn = sqlite3.connect('PackingListCreator.db')
 
 print("Created Database successfully")
+
+conn.execute("""
+        CREATE TABLE PRODUCT
+        (item_id INT Primary KEY NOT NULL,
+        nsn            TEXT NOT NULL,
+        nomenclature   TEXT NOT NULL,
+        unit_of_issue  CHAR(2));
+        """)
+
+print("Table created successfully")
