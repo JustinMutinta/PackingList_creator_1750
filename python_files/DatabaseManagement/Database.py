20c4f44d453a06320aba5fa9ccc52c14423b7936
justinmutinta <justinmutinta@hotmail.com>
2023-10-06 21:11:10 -0500

    Added value to table

--- python_files/DatabaseManagement/Database.py
+++ python_files/DatabaseManagement/Database.py
@@ -4,12 +4,19 @@ conn = sqlite3.connect('PackingListCreator.db')
 
 print("Created Database successfully")
 
-conn.execute("""
-        CREATE TABLE PRODUCT
-        (item_id INT Primary KEY NOT NULL,
-        nsn            TEXT NOT NULL,
-        nomenclature   TEXT NOT NULL,
-        unit_of_issue  CHAR(2));
-        """)
+# conn.execute("""
+#         CREATE TABLE PRODUCT
+#         (item_id INT Primary KEY NOT NULL,
+#         nsn            TEXT NOT NULL,
+#         nomenclature   TEXT NOT NULL,
+#         unit_of_issue  CHAR(2));
+#         """)
+#
+# print("Table created successfully")
 
-print("Table created successfully")
+conn.execute("insert into product (item_id, nsn, nomenclature, unit_of_issue) values (1, '6625-01-234-5678', 'Box', 'EA')")
+
+conn.commit()
+print("Records created successfully")
+
+conn.close()
