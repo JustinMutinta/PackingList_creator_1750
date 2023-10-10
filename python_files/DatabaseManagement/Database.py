b26a1de3e19f5d22fc2487c66bb486c06d1894a7
justinmutinta <justinmutinta@hotmail.com>
2023-10-10 16:43:04 -0500

    Added comments

--- python_files/DatabaseManagement/Database.py
+++ python_files/DatabaseManagement/Database.py
@@ -1,10 +1,12 @@
 import sqlite3
 from csvtranslator import csv_translator
 
+# creates database if its not available
 conn = sqlite3.connect('PackingListCreator.db')
 
 print("Created Database successfully")
 
+# To create the table
 # conn.execute("""
 #         CREATE TABLE PRODUCT
 #         (item_id INT Primary KEY NOT NULL,
@@ -15,22 +17,27 @@ print("Created Database successfully")
 #
 # print("Table created successfully")
 
+# To add a single manuel entry
 # conn.execute("insert into product (item_id, nsn, nomenclature, unit_of_issue) values (1, '6625-01-234-5678', 'Box', 'EA')")
 #
 # conn.commit()
 # print("Records created successfully")
 
-
+# To print out all contents of the table "products"
 cursor = conn.execute("select * from product")
 print(cursor.fetchall())
 
 
+#Print random record from the csv that has all the records
 # db_object = csv_translator()
 # print(db_object.return_rand_record())
 
+# Prints only the fields we are interested in and cuts out the rest.
 # db_object.set_object_details()
 # print(db_object.nsn, db_object.nomenclature, db_object.unit_of_issue)
 
+
+# Adds records to the database from item_id 2 to 10
 # for num in range(2,10):
 #     db_object.set_object_details()
 #     output_string = f"insert into product (item_id, nsn, nomenclature, unit_of_issue) values ({num}, '{db_object.nsn}', '{db_object.nomenclature}', '{db_object.unit_of_issue}')"
@@ -39,10 +46,11 @@ print(cursor.fetchall())
 #     print(f"{db_object.nomenclature} was entered")
 
 
+# Adds randoms records to the database
 # for num in range(10, 30):
 #     conn.execute(db_object.return_sql_string_for_table_entry(num))
 #     conn.commit()
 
 
-
+# Always close your database
 conn.close()
--- python_files/DatabaseManagement/csvtranslator.py
+++ python_files/DatabaseManagement/csvtranslator.py
@@ -2,11 +2,14 @@ import csv
 import random
 
 
+# Create class
 class csv_translator:
+    # Create variables
     nsn = ""
     nomenclature = ""
     unit_of_issue = ""
 
+    # Returns 20 random records from the csv file
     def return_20_records(self):
         record_return = []
         with open('nsn-extract.csv', 'r') as csv_file:
@@ -17,6 +20,7 @@ class csv_translator:
 
         return record_return
 
+    # Returns one random record from the csv file
     def return_rand_record(self):
         with open('nsn-extract.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -24,6 +28,7 @@ class csv_translator:
             rows = list(csv_reader)
             return rows[num][0], rows[num][2], rows[num][5]
 
+    # Sets one object variables
     def set_object_details(self):
         with open('nsn-extract.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -34,6 +39,7 @@ class csv_translator:
             self.nomenclature = rows[num][2]
             self.unit_of_issue = rows[num][5]
 
+    # Returns a string you can use to enter the record into the datbase
     def return_sql_string_for_table_entry(self, item_id):
         with open('nsn-extract.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
