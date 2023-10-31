bca042bca002e4e2f29bc76e0edba562e6fb8331
justinmutinta <justinmutinta@hotmail.com>
2023-10-30 20:06:02 -0500

    Able to get items from one box to the next

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 app = Flask(__name__)
 
-test = "Something to add"
+test = ["Something to add", "Something else to add", "Third thing worth add"]
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/1750")
+
+@app.route("/1750", methods=["GET", "POST"])
 def create_1750():
     conn = sqlite3.connect('/Users/justinmutinta/Documents/Python/PackingList_creator_1750_from_BidDawg/python_files/DatabaseManagement/PackingListCreator.db')
     cur = conn.cursor()
     cur.execute("SELECT * FROM product")
     data = cur.fetchall()
     cur.close()
+    if request.method == "POST":
+        to_add = request.form["label1"]
+        test.append(to_add)
     return render_template('html_files/1750_Generator.html', data = data, test = test)
+
 
 
 @app.route("/17501")
